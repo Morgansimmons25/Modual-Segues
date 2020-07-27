05daf5e56d8db820180630f5e431b4bdfa8b026a
Morgan Simmons <morgansimmons2525@gmail.com>
2020-07-27 14:07:40 -0500

    Initial Commit

---
+++ Modual Segues/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Modual Segues
+//
+//  Created by Student on 7/27/20.
+//  Copyright © 2020 Morgan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
